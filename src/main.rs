3b692952cf5a9274fe1b2704ec96e37e5e7cd6fb
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-11-02 18:43:19 -0500

    Feature: Parsing package and version string
    The commits that I plan to process will have the package and version
    strings embedded in them like:
    
    ```
     Package: changelog-generator@0.1.0
    ```
    
    This change processes strings formatted like this, and handles malformed
    strings.
    
    Note that I should also enforce that "Package" has to be at the
    beginning of the line.
    
    Package: changelog-generator@0.1.0
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,45 @@ use std::path::PathBuf;
 
 fn walk_git_history(path: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
     let repo = git2::Repository::discover(path)?;
+
     let mut revwalk = repo.revwalk()?;
     revwalk.push_head()?;
     revwalk.set_sorting(git2::Sort::TIME)?;
     for rev in revwalk {
         let commit = repo.find_commit(rev?)?;
-        let message = commit
-            .summary_bytes()
-            .unwrap_or_else(|| commit.message_bytes());
-        let body = commit
-            .body_bytes()
-            .unwrap_or_else(|| "<Empty Body>".as_bytes());
+        let message = String::from_utf8_lossy(
+            commit
+                .summary_bytes()
+                .unwrap_or_else(|| commit.message_bytes()),
+        );
+        let body = String::from_utf8_lossy(
+            commit
+                .body_bytes()
+                .unwrap_or_else(|| "<Empty Body>".as_bytes()),
+        );
 
-        println!("{}", commit.id());
-        println!("{}", String::from_utf8_lossy(message));
-        println!("{}\n\n", String::from_utf8_lossy(body));
+        println!("ID: {}", commit.id());
+        println!("Message: {}", message);
+
+        body.split('\n').for_each(|i| {
+            if i.contains("Package:") {
+                let mut meta = i.split(|c| c == ':' || c == '@');
+                let _ = meta.next().unwrap();
+                let package: String = meta
+                    .next()
+                    .unwrap_or_else(|| "")
+                    .split_whitespace()
+                    .collect();
+                let version: String = meta
+                    .next()
+                    .unwrap_or_else(|| "")
+                    .split_whitespace()
+                    .collect();
+                println!("Package: {}", package);
+                println!("Version: {}", version);
+                println!("\n\n");
+            }
+        });
     }
     Ok(())
 }
