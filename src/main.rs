7452a7a5705611080907472b3c68adef1a23dbe9
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-11-02 17:08:13 -0500

    Demonstrated: Git commit traversal
    Package: changelog-generator@0.1.0
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use git2::Repository;
+use std::path::PathBuf;
+
+fn walk_git_history(path: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+    let repo = git2::Repository::discover(path)?;
+    let mut revwalk = repo.revwalk()?;
+    revwalk.push_head()?;
+    revwalk.set_sorting(git2::Sort::TIME)?;
+    for rev in revwalk {
+        let commit = repo.find_commit(rev?)?;
+        println!("{}", commit.id());
+    }
+    Ok(())
+}
 
 fn main() {
-    let repo = match Repository::open("/home/sam/git/changelog-generator") {
-        Ok(repo) => repo,
-        Err(e) => panic!("failed to open: {}", e),
-    };
-
-    println!("{:?}", repo.state());
-    let oid = match git2::Oid::from_str("77b0c17a291c568d31534b3cb37732c2f3c54f7e") {
-        Ok(oid) => oid,
-        Err(e) => panic!("asdf {}", e),
-    };
-    println!("{:?}",repo.find_commit(oid));
+    walk_git_history(PathBuf::from(r"/home/sam/git/changelog-generator")).unwrap();
 }
