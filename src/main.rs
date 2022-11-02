66d42d336db728be128e9ddbe6538f51b32bf279
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-11-02 17:13:04 -0500

    Feature: Print out message and body text
    Package: changelog-generator@0.1.0
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,16 @@ fn walk_git_history(path: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
     revwalk.set_sorting(git2::Sort::TIME)?;
     for rev in revwalk {
         let commit = repo.find_commit(rev?)?;
+        let message = commit
+            .summary_bytes()
+            .unwrap_or_else(|| commit.message_bytes());
+        let body = commit
+            .body_bytes()
+            .unwrap_or_else(|| "<Empty Body>".as_bytes());
+
         println!("{}", commit.id());
+        println!("{}", String::from_utf8_lossy(message));
+        println!("{}\n\n", String::from_utf8_lossy(body));
     }
     Ok(())
 }
